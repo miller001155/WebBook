bd3ac219d3f5c1125a818ef81f18bf5f13d0bc99
miller001155 <miller00115@gmail.com>
2022-11-05 17:04:00 +0300

    9

--- catalog/admin.py
+++ catalog/admin.py
@@ -3,18 +3,32 @@ from .models import Author, Book, Genre, Language, Status, BookInstance
 
 
 class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')  # дополнительная информация
+    list_display = ('last_name', 'first_name')  # дополнительная информация
+    fields = ['last_name', 'first_name', ('date_of_birth', 'date_of_death')]  # изменение расположения
+
+
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
 
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'genre', 'language', 'display_author')  # дополнительная информация
     list_filter = ('genre', 'author')  # фильтрация в панели администратора
+    inlines = [BookInstanceInline]
 
 
 @admin.register(BookInstance)  # заменяет admin.site.register
 class BookInstanceAdmin(admin.ModelAdmin):
     list_filter = ('book', 'status')  # фильтрация в панели администратора
+    fieldsets = (
+        ('Экземпляр книги', {
+            'fields': ('book', 'imprint', 'inv_nom')
+        }),
+        ('Статус и окончание его действия', {
+            'fields': ('status', 'due_back')
+        })
+    )
 
 
 # admin.site.register(Author)
